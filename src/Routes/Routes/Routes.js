74d4792fd86cf6dc67778c38929b7850e339328e
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-11-24 14:28:03 +0600

    first commit

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,24 @@
+import Login from "../../Pages/login/Login";
+
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../../Layout/Main");
+const { default: Home } = require("../../Pages/Home/Home/Home");
+
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<Main></Main>,
+        children:[
+            {
+                path:"/",
+                element:<Home/>
+            },
+            {
+                path:"/login",
+                element:<Login/>
+            },
+
+        ]
+    }
+])
